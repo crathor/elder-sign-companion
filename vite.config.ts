3a211f8182027f8c048af749383191dfb7a5489f
Cody Rathor <codyrathor@Codys-MacBook-Pro.local>
2023-01-16 23:00:20 -0800

    chore: add gh-pages, add base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/elder-sign-companion",
+    base: "/elder-sign-companion/",
     plugins: [react()],
 });
