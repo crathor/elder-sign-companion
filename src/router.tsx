6fc621c0a55974c9fdf900b2eccd76c88f4e280c
Cody Rathor <codyrathor@Codys-MacBook-Pro.local>
2023-01-19 01:17:28 -0800

    remove unused React import

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./routes/Root";
 import InvestigatorPage, { loader as investigatorLoader } from "./routes/Investigator";
